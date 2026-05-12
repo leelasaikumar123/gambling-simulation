a3abe46bc054578f7b7ff52bf616bbeaa428b1b6
leelasaikumar123 <leelasaikumarmeda501@gmail.com>
2026-05-12 09:35:23 +0530

    feat:feature that calculates amount he left with in a day

--- GamblingSimulation.java
+++ GamblingSimulation.java
@@ -3,17 +3,20 @@ import java.util.Random;
 public class GamblingSimulation{
     public static Random random=new Random();
     public static void main(String[] args) {
-        profitOrLossInADay();
+        System.out.println(profitOrLossInADay());
     }
     public static int profitOrLossInADay(){
         int stake=100;
         int bet=1;
         int outcome=random.nextInt(2);
-  if(outcome==0){
-    System.out.println("He Lost The Bet");
+while(stake != 50 && stake != 150){
+      if(outcome==0){
+    stake--;
   }
   else{
-    System.out.println("He won the bet");
+    stake++;
   }
+}
+return stake;
     }
 }
