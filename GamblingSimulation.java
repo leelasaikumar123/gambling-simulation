45673e8e7d89a656b159ce4f5d9638fae88a8fe0
leelasaikumar123 <leelasaikumarmeda501@gmail.com>
2026-05-12 07:59:36 +0530

    feat:user starts with stake of 100 everyday and 1 bet

---
+++ GamblingSimulation.java
@@ -0,0 +1,9 @@
+public class GamblingSimulation{
+    public static void main(String[] args) {
+        
+    }
+    public static int profitOrLossInADay(){
+        int stake=100;
+        int bet=1;
+    }
+}
