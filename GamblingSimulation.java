1a22cce615a0fc0eb5b9d29244437a8ebece09f2
leelasaikumar123 <leelasaikumarmeda501@gmail.com>
2026-05-12 09:27:34 +0530

    feat:added feature that tells weather gambler won the bet or lost

--- GamblingSimulation.java
+++ GamblingSimulation.java
@@ -1,9 +1,19 @@
+import java.util.Random;
+
 public class GamblingSimulation{
+    public static Random random=new Random();
     public static void main(String[] args) {
-        
+        profitOrLossInADay();
     }
     public static int profitOrLossInADay(){
         int stake=100;
         int bet=1;
+        int outcome=random.nextInt(2);
+  if(outcome==0){
+    System.out.println("He Lost The Bet");
+  }
+  else{
+    System.out.println("He won the bet");
+  }
     }
 }
